0c0feaae3deb34dce28cecc76ade18eb36e80b50
pavel.nenia <pavel.nenya228@gmail.com>
2024-03-27 23:42:09 +0200

    Add export to database rake task

---
+++ 2/Prac/test/lib/tasks/export_to_db_test.rb
@@ -0,0 +1,23 @@
+require 'test_helper'
+require 'rake'
+
+class ParseLinksTaskTest < ActiveSupport::TestCase
+  def setup
+    @rake = Rake::Application.new
+    Rake.application = @rake
+    Rails.application.load_tasks
+  end
+
+  test "parse:links task should parse and save links" do
+    # Ми можемо використовувати стаби для вхідних даних для тестування
+    stub_request(:get, "https://www.nerdwallet.com/article/small-business/service-business-ideas")
+      .to_return(body: File.read('test/fixtures/example.html'))
+
+    out, _ = capture_io do
+      Rake::Task["parse:links"].invoke
+    end
+
+    assert_match(/Знайдено посилання:/, out)
+    # Додайте тут подальші перевірки для перевірки збереження посилань у базі даних
+  end
+end
